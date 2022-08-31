4ff212034ce8ca0c01b0d24cbccde93371df8eb4
Kuvshinov Vlad <kuvshinov_v@mail.ru>
2022-08-31 10:45:37 +0300

    Добавил ПЕРВОЕ решение второй задачи, оно длинное без цикла но работает... т.к. ни как не мог приладить к этому делу цикл...

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16 */
 
-void Stepen(int first, int second){
+/* void Stepen(int first, int second){
     int result = 1;
     for (int i = 1; i <= second; i++)
     {
@@ -18,7 +18,7 @@ Console.WriteLine("Введите значение степени, в котор
 int snd = Convert.ToInt32(Console.ReadLine());
 
 Stepen(fst,snd);
-
+ */
 
 
 
@@ -28,6 +28,141 @@ Stepen(fst,snd);
 82 -> 10
 9012 -> 12 */
 
+
+void Summa(int chislo){
+
+        if (chislo >= 1 && chislo <= 9){
+        Console.WriteLine("Сумма цифр в числе равна: " + chislo);
+    }
+    
+    else if (chislo >= 10 && chislo <= 99){
+        int first = chislo / 10;
+        int second = chislo % 10;
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} = {first+second}");
+    }
+    else if(chislo >= 100 && chislo <= 999){
+        int first = chislo / 100;
+        int secondtmp = chislo / 10;
+        int second = secondtmp % 10;
+        int threeth = chislo % 10;
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} + {threeth} = {first+second+threeth}");
+    }
+    else if(chislo >= 1000 && chislo <= 9999){
+        int first = chislo / 1000;
+        int secondtmp = chislo / 100;
+        int second = secondtmp % 10;
+        int threethtmp = chislo / 10;
+        int threeth = threethtmp % 10;
+        int fourth = chislo % 10;
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} + {threeth} + {fourth} = {first+second+threeth+fourth}");
+    }
+    else if(chislo >= 10000 && chislo <= 99999){
+        int first = chislo / 10000;
+        int secondtmp = chislo / 1000;
+        int second = secondtmp % 10;
+        int threethtmp = chislo / 100;
+        int threeth = threethtmp % 10;
+        int fourthtmp = chislo / 10;
+        int fourth = fourthtmp % 10;
+        int fifth = chislo % 10;
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} + {threeth} + {fourth} + {fifth} = {first+second+threeth+fourth+fifth}");
+    }
+    else if(chislo >= 100000 && chislo <= 999999){
+        int first = chislo / 100000;
+        int secondtmp = chislo / 10000;
+        int second = secondtmp % 10;
+        int threethtmp = chislo / 1000;
+        int threeth = threethtmp % 10;
+        int fourthtmp = chislo / 100;
+        int fourth = fourthtmp % 10;
+        int fifthtmp = chislo / 10;
+        int fifth = fifthtmp % 10;
+        int sixth = chislo % 10;
+
+
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} + {threeth} + {fourth} + {fifth} + {sixth} = {first+second+threeth+fourth+fifth+sixth}");
+    }
+    else if(chislo >= 1000000 && chislo <= 9999999){
+        int first = chislo / 1000000;
+        int secondtmp = chislo / 100000;
+        int second = secondtmp % 10;
+        int threethtmp = chislo / 10000;
+        int threeth = threethtmp % 10;
+        int fourthtmp = chislo / 1000;
+        int fourth = fourthtmp % 10;
+        int fifthtmp = chislo / 100;
+        int fifth = fifthtmp % 10;
+        int sixthtmp = chislo / 10;
+        int sixth = sixthtmp % 10;
+        int seventh = chislo % 10;
+
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} + {threeth} + {fourth} + {fifth} + {sixth} + {seventh} = {first+second+threeth+fourth+fifth+sixth+seventh}");
+    }
+    else if(chislo >= 10000000 && chislo <= 99999999){
+        int first = chislo / 10000000;
+        int secondtmp = chislo / 1000000;
+        int second = secondtmp % 10;
+        int threethtmp = chislo / 100000;
+        int threeth = threethtmp % 10;
+        int fourthtmp = chislo / 10000;
+        int fourth = fourthtmp % 10;
+        int fifthtmp = chislo / 1000;
+        int fifth = fifthtmp % 10;
+        int sixthtmp = chislo / 100;
+        int sixth = sixthtmp % 10;
+        int seventhtmp = chislo / 10;
+        int seventh = seventhtmp % 10;
+        int eighth = chislo % 10;
+
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} + {threeth} + {fourth} + {fifth} + {sixth} + {seventh} + {eighth} = {first+second+threeth+fourth+fifth+sixth+seventh+eighth}");
+    }
+    else if(chislo >= 100000000 && chislo <= 999999999){
+        int first = chislo / 100000000;
+        int secondtmp = chislo / 10000000;
+        int second = secondtmp % 10;
+        int threethtmp = chislo / 1000000;
+        int threeth = threethtmp % 10;
+        int fourthtmp = chislo / 100000;
+        int fourth = fourthtmp % 10;
+        int fifthtmp = chislo / 10000;
+        int fifth = fifthtmp % 10;
+        int sixthtmp = chislo / 1000;
+        int sixth = sixthtmp % 10;
+        int seventhtmp = chislo / 100;
+        int seventh = seventhtmp % 10;
+        int eighthtmp = chislo / 10;
+        int eighth = eighthtmp % 10;
+        int ninth = chislo % 10;
+
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} + {threeth} + {fourth} + {fifth} + {sixth} + {seventh} + {eighth} + {ninth} = {first+second+threeth+fourth+fifth+sixth+seventh+eighth+ninth}");
+    }
+    else if(chislo >= 1000000000 && chislo <= 999999999){
+        int first = chislo / 1000000000;
+        int secondtmp = chislo / 100000000;
+        int second = secondtmp % 10;
+        int threethtmp = chislo / 10000000;
+        int threeth = threethtmp % 10;
+        int fourthtmp = chislo / 1000000;
+        int fourth = fourthtmp % 10;
+        int fifthtmp = chislo / 100000;
+        int fifth = fifthtmp % 10;
+        int sixthtmp = chislo / 1000;
+        int sixth = sixthtmp % 10;
+        int seventhtmp = chislo / 1000;
+        int seventh = seventhtmp % 10;
+        int eighthtmp = chislo / 100;
+        int eighth = eighthtmp % 10;
+        int ninthtmp = chislo / 10;
+        int ninth = ninthtmp % 10;
+        int tenth = chislo % 10;
+
+        Console.WriteLine($"Сумма цифр в числе равна: {first} + {second} + {threeth} + {fourth} + {fifth} + {sixth} + {seventh} + {eighth} + {ninth} + {tenth} = {first+second+threeth+fourth+fifth+sixth+seventh+eighth+ninth+tenth}");
+    }
+  
+} 
+Console.WriteLine("Введите число: ");
+int chislo = Convert.ToInt32(Console.ReadLine());
+Summa(chislo);
 
 
 
